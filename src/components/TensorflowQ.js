83e828fdb94bc2c231d7de16acead8f94fd68a5d
Prabharoop <33201083+Prabharoop@users.noreply.github.com>
2020-08-25 16:28:53 +0530

    multiple-fixes

--- src/components/TensorflowQ.js
+++ src/components/TensorflowQ.js
@@ -12,7 +12,6 @@ export default class TensorflowQ extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isTfReady: false,
             isModelReady: false,
             default_question: "Who is the CEO of Google?",
             default_passage: "Google LLC is an American multinational technology company that specializes in Internet-related services and products, which include online advertising technologies, search engine, cloud computing, software, and hardware. It is considered one of the Big Four technology companies, alongside Amazon, Apple, and Facebook. Google was founded in September 1998 by Larry Page and Sergey Brin while they were Ph.D. students at Stanford University in California. Together they own about 14 percent of its shares and control 56 percent of the stockholder voting power through supervoting stock. They incorporated Google as a California privately held company on September 4, 1998, in California. Google was then reincorporated in Delaware on October 22, 2002. An initial public offering (IPO) took place on August 19, 2004, and Google moved to its headquarters in Mountain View, California, nicknamed the Googleplex. In August 2015, Google announced plans to reorganize its various interests as a conglomerate called Alphabet Inc. Google is Alphabet's leading subsidiary and will continue to be the umbrella company for Alphabet's Internet interests. Sundar Pichai was appointed CEO of Google, replacing Larry Page who became the CEO of Alphabet.",
@@ -28,8 +27,6 @@ export default class TensorflowQ extends Component {
     }
 
     async componentDidMount() {
-        await tf.ready(); // preparing TensorFlow
-        this.setState({ isTfReady: true, });
 
         this.model = await qna.load(); // preparing MobileBERT model qna
         this.setState({ isModelReady: true });
@@ -63,7 +60,7 @@ export default class TensorflowQ extends Component {
 
     render() {
 
-        const { isTfReady, isModelReady, passage, question, answers } = this.state
+        const { isModelReady, passage, question, answers } = this.state
 
         const onPress = (passage, question) => {
             this.findAnswers(passage, question).then((the_answers) => {
@@ -99,7 +96,6 @@ export default class TensorflowQ extends Component {
                 </div>
                 <div className='qna-container'>
                     <b>
-                        TensorFlow.js ready? {isTfReady ? <p>✅</p> : 'Not ready'}
                         BERT model qna ready?
             {isModelReady ?
                             <p>✅</p> : 'Not yet'
